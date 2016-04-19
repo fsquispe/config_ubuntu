ad25dd3013fddb99f256c65d172c75794acb4e1a
Fredy Quispe <fredysqm@gmail.com>
2016-04-19 12:31:53 -0500

    stable: nginx

--- xenial/nginx.sh
+++ xenial/nginx.sh
@@ -11,10 +11,11 @@ read -p "ip principal: " MY_IP
 apt -y install nginx
 systemctl stop nginx
 cp ./tmpl/nginx/nginx.conf /etc/nginx/
+mkdir /etc/nginx/global.d/
 cp ./tmpl/nginx/global.d/custom.conf /etc/nginx/global.d/
-cp ./tmpl/nginx/sites-available/default /etc/nginx/sites-available/
-cp ./tmpl/nginx/sites-available/default-ssl /etc/nginx/sites-available/
-cp ./tmpl/nginx/sites-available/redirect /etc/nginx/sites-available/
+sed -e s/"MY_IP"/"$MY_IP"/g ./tmpl/nginx/sites-available/default > /etc/nginx/sites-available/default
+sed -e s/"MY_IP"/"$MY_IP"/g ./tmpl/nginx/sites-available/default-ssl > /etc/nginx/sites-available/default-ssl
+sed -e s/"MY_IP"/"$MY_IP"/g ./tmpl/nginx/sites-available/redirect > /etc/nginx/sites-available/redirect
 rm /etc/nginx/sites-enabled/*
 ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
 ln -s /etc/nginx/sites-available/default-ssl /etc/nginx/sites-enabled/
@@ -26,7 +27,7 @@ mkdir /root/bin/
 mkdir /root/tmpl/
 cp ./tmpl/nginx/root/bin/create_user /root/bin/
 cp ./tmpl/nginx/root/bin/delete_user /root/bin/
-cp ./tmpl/nginx/root/tmpl/vhost.conf /root/tmpl/
+sed -e s/"MY_IP"/"$MY_IP"/g ./tmpl/nginx/root/tmpl/vhost.conf > /root/tmpl/vhost.conf
 chmod 500 /root/bin/*
 chmod 400 /root/tmpl/*
 #EOF
