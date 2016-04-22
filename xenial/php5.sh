22ef95ba3848088f186d8f270718265b7da07069
Fredy Quispe <fredysqm@gmail.com>
2016-04-22 19:20:51 +0000

    php5.sh edited online with Bitbucket

--- xenial/php5.sh
+++ xenial/php5.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 ###############################################################################
-##### PHP5-FPM Y CLI ##########################################################
+##### PHP7-FPM Y CLI ##########################################################
 ###############################################################################
-apt -y install php5-fpm php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-memcached php5-ming php5-ps php5-pspell php5-recode php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-json
+apt -y php7.0 php7.0-curl php7.0-mysql php7.0-gd php7.0-intl php7.0-imap php7.0-mcrypt php7.0-recode php7.0-sqlite3 php7.0-pspell php7.0-xml php7.0-xmlrpc php7.0-xsl php7.0-tidy php7.0-zip php7.0-soap php7.0-mbstring php-pear php-memcache php-memcached php-imagick
 php5enmod imap
 php5enmod mcrypt
 php5enmod ps
 php5enmod readline
 
-cd /etc/php5/fpm/
+cd /etc/php7/fpm/
 wget -N https://dl.dropboxusercontent.com/u/1201303/trusty.3/tmpl/php.ini
 cp php.ini /etc/php5/cli/php.ini
 rm /etc/php5/fpm/pool.d/*
