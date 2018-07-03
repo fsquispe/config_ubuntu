239ebf57e1237ed2366e778102995c1d46cb2e3d
Fredy Quispe <fredysqm@gmail.com>
2018-07-02 23:40:40 -0500

    bionic rc2

--- django-uwsgi.sh
+++ django-uwsgi.sh
@@ -4,7 +4,7 @@
 ### PYTHON - DJANGO - UWSGI  ##################################################
 ###############################################################################
 apt -y install libmysqlclient-dev libssl-dev libcrypto++-dev libffi-dev
-apt -y install python-dev python-mysqldb python-pip python-virtualenv pypy pypy-dev pypy-lib python3-pip python3-dev
+apt -y install virtualenv python-dev python-mysqldb python-pip python-virtualenv pypy pypy-dev pypy-lib python3-pip python3-dev
 apt -y install nodejs npm
 ln -s /usr/bin/nodejs /usr/local/bin/node
 npm install -g stylus
