0605e57310f8c5b88acd464fbef1b8269b1368bb
Braulio Soncco <soncco@gmail.com>
2020-01-01 17:16:48 -0500

    Last version of node

--- django-uwsgi.sh
+++ django-uwsgi.sh
@@ -5,6 +5,10 @@
 ###############################################################################
 apt -y install libmysqlclient-dev libssl-dev libcrypto++-dev libffi-dev
 apt -y install virtualenv python-dev python-mysqldb python-pip python-virtualenv pypy pypy-dev pypy-lib python3-pip python3-dev
+
+# NODE #
+sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt -y install nodejs npm
 ln -s /usr/bin/nodejs /usr/local/bin/node
 npm install -g stylus
