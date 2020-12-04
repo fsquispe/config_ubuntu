e8312be043b83d45989a98915fefaf847a35592a
Fredy Quispe <fredysqm@gmail.com>
2020-12-04 10:16:02 -0500

    beta4

--- php74.sh
+++ php74.sh
@@ -3,10 +3,10 @@
 ###############################################################################
 ##### PHP7.4 -FPM Y CLI #######################################################
 ###############################################################################
-apt install -y php7.2-cli php7.2-fpm php7.2-common php7.2-json php7.2-opcache php7.2-readline php7.2-curl php7.2-mysql php7.2-gd php7.2-intl php7.2-imap php7.2-recode php7.2-sqlite3 php7.2-xml php7.2-xmlrpc php7.2-xsl php7.2-tidy php7.2-zip php7.2-soap php7.2-mbstring php-pear php-memcache php-memcached php-imagick
+apt install -y php7.4-cli php7.4-fpm php7.4-common php7.4-json php7.4-opcache php7.4-readline php7.4-curl php7.4-mysql php7.4-gd php7.4-intl php7.4-imap php7.4-sqlite3 php7.4-xml php7.4-xmlrpc php7.4-xsl php7.4-tidy php7.4-zip php7.4-soap php7.4-mbstring php-pear php-memcache php-memcached php-imagick
 ##### CONFIGURACION ###########################################################
-cp ./tmpl/php72/php.ini /etc/php/7.2/cli/php.ini
-cp /etc/php/7.2/cli/php.ini /etc/php/7.2/fpm/php.ini
-rm /etc/php/7.2/fpm/pool.d/*
-systemctl restart php7.2-fpm
+cp ./tmpl/php74/php.ini /etc/php/7.4/cli/php.ini
+cp /etc/php/7.4/cli/php.ini /etc/php/7.4/fpm/php.ini
+rm /etc/php/7.4/fpm/pool.d/*
+systemctl restart php7.4-fpm
 #EOF
