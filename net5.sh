44305fe209be4dccab004587570190077cbce554
Fredy Quispe <fredysqm@gmail.com>
2021-03-31 09:06:16 -0500

    versión abril2021

---
+++ net5.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+###############################################################################
+### CONFIGURADOR NET5 Y SQL SERVER ############################################
+###############################################################################
+cd /tmp
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+apt install -y apt-transport-https
+apt update -y
+apt -y install dotnet-sdk-5.0
+apt -y install dotnet-runtime-5.0
+apt -y install unixodbc unixodbc-dev msodbcsql17
+#EOF
