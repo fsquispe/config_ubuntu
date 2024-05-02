d25ea060c8a9bd0e7d93d37a37f4e7d88d920df2
Fredy Quispe Medina <fredysqm@users.noreply.github.com>
2024-05-02 11:42:19 -0500

    Update net6.sh

--- net6.sh
+++ net6.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 ###############################################################################
-### CONFIGURADOR NET6 Y SQL SERVER ############################################
+### CONFIGURADOR NET8 Y SQL SERVER ############################################
 ###############################################################################
 apt install -y apt-transport-https
 apt update -y
-apt -y install dotnet-sdk-6.0
-apt -y install dotnet-runtime-6.0
+apt -y install dotnet-sdk-8.0
 apt -y install unixodbc unixodbc-dev
 #EOF
