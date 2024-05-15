a1a499b3cc63600a68f637d1ddf36fc29f978b90
Fredy Quispe Medina <desarrollador02@else.com.pe>
2024-05-15 14:58:18 -0500

    versión 20240515

--- python.sh
+++ python.sh
@@ -5,5 +5,5 @@
 ###############################################################################
 apt -y install python-is-python3
 apt -y install libmysqlclient-dev libssl-dev libcrypto++-dev libffi-dev
-apt -y install virtualenv python3-mysqldb python3-virtualenv pypy pypy-dev pypy-lib python3-pip python3-dev
+apt -y install virtualenv python3-mysqldb python3-virtualenv python3-pip python3-dev
 #EOF
